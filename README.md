4b3243dbd52fd1300cbe7ac4b182c8dd3acf9bce
JeonJoonyong <drtkdldjstm@naver.com>
2022-05-24 17:04:49 +0900

    update code

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Django lotto tutorial
 [[Django] lotto 번호 자동생성 웹 서비스 (02. 웹 구현하기)](https://jeonjoon.tistory.com/56)
 
 [관리자(admin)페이지 모델객체 추가 안될 때(settings.py / INSTALLED_APPS)](https://jeonjoon.tistory.com/55)
+
--- site_1/lotto/views.py
+++ site_1/lotto/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import GuessNumbers
+from .forms import PostForm
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hello, world!</h1>')
+    lottos = GuessNumbers.objects.all()
+    # 브라우저로부터 넘어온 request를 그대로 template('default.html')에게 전달
+    # {} 에는 추가로 함께 전달하려는 object들을 dict로 넣어줄 수 있음
+    return render(request, 'lotto/default.html', {'lottos':lottos})
+
 
 def hello(request):
     return HttpResponse("<h1 style='color:red;'>Hello, world!")
 
+
+def post(request):
+    form = PostForm()
+    return render(request,"lotto/form.html",{"form":form})
